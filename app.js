ef13c6e4b7f435b4b4dc5bacce91102d8f572aa9
ronee-muhammad-rayhan <muhammad@rayhanworld.com>
2023-10-01 22:24:16 +0600

    mongoose added with success message

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const router = require('./src/Routes/api');
 const app = new express();
 const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
+// import MongoStore from 'connect-mongo';
 
 //Security Middleware
 const rateLimit = require('express-rate-limit');
@@ -32,12 +33,21 @@ const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 3000 })
 app.use(limiter)
 
 // Mongo DB Database Connection
-let URI = 'mongodb://localhost:27017';
-let OPTION = { user: 'test', pass: 'test1234', autoIndex: true }
-mongoose.connect(URI, OPTION, (error) => {
-    console.log("Connection Success")
-    console.log(error)
-})
+// let URI = 'mongodb://localhost:27017';
+// let OPTION = { user: 'test', pass: 'test1234', autoIndex: true }
+// mongoose.connect(URI, OPTION, (error) => {
+//     console.log("Connection Success")
+//     console.log(error)
+// })
+
+
+
+
+mongoose.connect("mongodb://localhost:27017", { useNewUrlParser: true, useUnifiedTopology: true })
+
+    .then(() => console.log('Connected Successfully'))
+
+    .catch((err) => { console.error(err); });
 
 // Routing Implement
 app.use("/api/v1", router)
