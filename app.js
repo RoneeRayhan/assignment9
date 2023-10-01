37976f1715cc59033a4b7ffa980c33e2b1b921a6
ronee-muhammad-rayhan <muhammad@rayhanworld.com>
2023-10-01 22:19:28 +0600

    mongoose added but bugs

--- app.js
+++ app.js
@@ -32,44 +32,12 @@ const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 3000 })
 app.use(limiter)
 
 // Mongo DB Database Connection
-// let URI = "mongodb+srv://<username>:<password>@cluster0.7uslu.mongodb.net/inven?retryWrites=true&w=majority";
-// let OPTION = { user: 'testuser7777', pass: 'testuser7777', autoIndex: true }
-// mongoose.connect(URI, OPTION, (error) => {
-//     console.log("Connection Success")
-//     console.log(error)
-// })
-
-// MongoDB connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database name
-const dbName = 'your-database-name'; // Replace with your desired database name
-
-async function connectToMongoDB() {
-    try {
-        const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-
-        // Connect to MongoDB
-        await client.connect();
-
-        // Add the database to the application
-        const db = client.db(dbName);
-
-        // Log a success message to the console
-        console.log(`Connected to MongoDB database '${dbName}'`);
-
-        // Do further operations with the database as needed
-
-        // Close the MongoDB connection when done
-        client.close();
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-    }
-}
-
-// Call the function to connect to MongoDB
-connectToMongoDB();
-
+let URI = 'mongodb://localhost:27017';
+let OPTION = { user: 'test', pass: 'test1234', autoIndex: true }
+mongoose.connect(URI, OPTION, (error) => {
+    console.log("Connection Success")
+    console.log(error)
+})
 
 // Routing Implement
 app.use("/api/v1", router)
